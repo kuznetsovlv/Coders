cac9c22334a473e0b11fa5b45de6fa00648c02a5
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2017-07-15 03:27:31 +0300

    Fixed error catch

--- src/coders/Coders.java
+++ src/coders/Coders.java
@@ -11,14 +11,16 @@ public class Coders {
     public static void main(String[] args) {
         byte[] byteList = {-128, 1, 1, 0, -1};
         
-        File file = new File(args[0]);
-        try(FileInputStream input = new FileInputStream(file)) {  
-            byteList = new byte[(int)file.length()];
-            
-            input.read(byteList);
-        } catch(Exception e) {             
-            System.err.println("e");
-            System.exit(1);
+        try {
+            File file = new File(args[0]);
+            try(FileInputStream input = new FileInputStream(file)) {  
+                byteList = new byte[(int)file.length()];  
+                input.read(byteList);
+            } catch(Exception e) {
+                throw e;
+            }
+        } catch (Exception e) {
+            System.err.println(e);
         } finally {
             System.out.println(CoderFactory.get(CoderFactory.CoderTypes.BASE64).code(byteList));
         }
